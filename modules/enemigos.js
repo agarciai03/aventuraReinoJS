0256a1ffd55ec29466ec827900e4d0813ce37b7d
Alberto García <albertogiz2005@gmail.com>
2025-11-24 23:01:05 +0100

    🔁Update JSDoc moudlo enemigos

--- modules/enemigos.js
+++ modules/enemigos.js
@@ -1,5 +1,13 @@
-// Archivo: modules/enemigos.js
+/**
+ * Clase que representa un enemigo normal.
+ */
 export class Enemigo {
+    /**
+     * @param {string} nombre - Nombre del enemigo
+     * @param {string} avatar - Imagen del enemigo
+     * @param {number} ataque - Nivel de ataque
+     * @param {number} vida - Vida del enemigo
+     */
     constructor(nombre, avatar, ataque, vida) {
         this.nombre = nombre;
         this.avatar = avatar;
@@ -7,11 +15,27 @@ export class Enemigo {
         this.vida = vida;
     }
 }
+
+/**
+ * Subclase para Jefes, hereda de enemigo.
+ */
 export class Jefe extends Enemigo {
+    /**
+     * @param {string} nombre
+     * @param {string} avatar
+     * @param {number} ataque
+     * @param {number} vida
+     * @param {number} multiplicador - Multiplicador de puntos/daño
+     */
     constructor(nombre, avatar, ataque, vida, multiplicador = 1.2) {
         super(nombre, avatar, ataque, vida);
         this.multiplicador = multiplicador;
     }
+
+    /**
+     * Calcula los puntos al derrotar al jefe.
+     * @returns {number}
+     */
     calcularPuntosDerrota() {
         return Math.round((100 + this.ataque) * this.multiplicador);
     }
