155b5bfc0016e554d9f4011d2727ee6f3e49df5e
Alberto García <albertogiz2005@gmail.com>
2025-11-24 22:57:41 +0100

    🔁Update JSDoc modulo mercado

--- modules/mercado.js
+++ modules/mercado.js
@@ -1,8 +1,10 @@
-// Archivo: modules/mercado.js
 import { productosMercado } from "../utils/constants.js";
 import { Producto } from "./producto.js";
 
-// Devuelve lista de productos como instancias de Producto
+/**
+ * Devuelve lista de productos como instancias de Producto.
+ * @returns {Producto[]} Array de productos instanciados.
+ */
 export function obtenerProductosMercado() {
     return productosMercado.map(obj => new Producto(obj.nombre, obj.precio, obj.rareza, obj.tipo, obj.bonus, obj.imagen));
 }
