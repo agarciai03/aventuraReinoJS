9aa6e45e7be5a1fd9507bf9bac3d30831f1f44e1
Alberto García <albertogiz2005@gmail.com>
2025-11-24 23:17:20 +0100

    🔥Fix Precios mas justos para el desarrollo del juego

--- utils/constants.js
+++ utils/constants.js
@@ -18,16 +18,16 @@ export const UMBRAL_VETERANO = 300;
  */
 /** Lista de productos disponibles en el mercado */
 export const productosMercado = [
-    { nombre: 'Espada corta', precio: 40, rareza: 'común', tipo: 'arma', bonus: { ataque: 8 }, imagen: 'img/axe.png' },
-    { nombre: 'Arco de caza', precio: 50, rareza: 'común', tipo: 'arma', bonus: { ataque: 7 }, imagen: 'img/b_t_01.png' },
-    { nombre: 'Armadura de cuero', precio: 65, rareza: 'común', tipo: 'armadura', bonus: { defensa: 6 }, imagen: 'img/armor.png' },
+    { nombre: 'Espada corta', precio: 30, rareza: 'común', tipo: 'arma', bonus: { ataque: 8 }, imagen: 'img/axe.png' },
+    { nombre: 'Arco de caza', precio: 40, rareza: 'común', tipo: 'arma', bonus: { ataque: 7 }, imagen: 'img/b_t_01.png' },
+    { nombre: 'Armadura de cuero', precio: 55, rareza: 'común', tipo: 'armadura', bonus: { defensa: 6 }, imagen: 'img/armor.png' },
     { nombre: 'Poción pequeña', precio: 15, rareza: 'común', tipo: 'consumible', bonus: { curacion: 20 }, imagen: 'img/apple.png' },
-    { nombre: 'Espada rúnica', precio: 135, rareza: 'raro', tipo: 'arma', bonus: { ataque: 18 }, imagen: 'img/axe.png' },
-    { nombre: 'Escudo de roble', precio: 120, rareza: 'raro', tipo: 'armadura', bonus: { defensa: 14 }, imagen: 'img/shield.png' },
-    { nombre: 'Poción grande', precio: 40, rareza: 'raro', tipo: 'consumible', bonus: { curacion: 60 }, imagen: 'img/hp.png' },
-    { nombre: 'Mandoble épico', precio: 210, rareza: 'épico', tipo: 'arma', bonus: { ataque: 32 }, imagen: 'img/axe.png' },
-    { nombre: 'Placas dracónicas', precio: 180, rareza: 'épico', tipo: 'armadura', bonus: { defensa: 28 }, imagen: 'img/helmets.png' },
-    { nombre: 'Elixir legendario', precio: 90, rareza: 'épico', tipo: 'consumible', bonus: { curacion: 150 }, imagen: 'img/hp.png' }
+    { nombre: 'Espada rúnica', precio: 105, rareza: 'raro', tipo: 'arma', bonus: { ataque: 18 }, imagen: 'img/axe.png' },
+    { nombre: 'Escudo de roble', precio: 100, rareza: 'raro', tipo: 'armadura', bonus: { defensa: 14 }, imagen: 'img/shield.png' },
+    { nombre: 'Poción grande', precio: 30, rareza: 'raro', tipo: 'consumible', bonus: { curacion: 60 }, imagen: 'img/hp.png' },
+    { nombre: 'Mandoble épico', precio: 180, rareza: 'épico', tipo: 'arma', bonus: { ataque: 32 }, imagen: 'img/axe.png' },
+    { nombre: 'Placas dracónicas', precio: 160, rareza: 'épico', tipo: 'armadura', bonus: { defensa: 28 }, imagen: 'img/helmets.png' },
+    { nombre: 'Elixir legendario', precio: 65, rareza: 'épico', tipo: 'consumible', bonus: { curacion: 150 }, imagen: 'img/hp.png' }
 ];
 
 /**
