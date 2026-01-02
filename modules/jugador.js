201206c8efddc7df9ddc918bdee688e06c379756
Alberto García <albertogiz2005@gmail.com>
2026-01-02 16:52:15 +0100

    🔁Update Nuevas propiedades para el jugador al crearlo desde el form

--- modules/jugador.js
+++ modules/jugador.js
@@ -4,17 +4,26 @@
 export class Jugador {
     /**
      * @param {string} nombre - Nombre del jugador
-     * @param {string} avatar - (opcional) Ruta imagen avatar
+     * @param {string} avatar - Ruta imagen avatar
      * @param {number} vidaMax - Vida máxima inicial
+     * @param {number} ataqueBase - Ataque base del formulario
+     * @param {number} defensaBase - Defensa base del formulario
      */
-    constructor(nombre, avatar = "img/personaje.png", vidaMax = 100) {
+    constructor(nombre, avatar = "img/personaje.png", vidaMax, ataqueBase, defensaBase) {
         this.nombre = nombre;
         this.avatar = avatar;
+
         this.vida = vidaMax;
         this.vidaMax = vidaMax;
+
+        this.ataqueBase = ataqueBase;
+        this.defensaBase = defensaBase;
+        this.oro = 500; 
+
         this.inventario = [];
         this.puntos = 0;
     }
+
     /**
      * Añade un objeto (producto) al inventario.
      * @param {Producto} objeto - Producto a añadir
@@ -22,6 +31,7 @@ export class Jugador {
     añadirAlInventario(objeto) {
         this.inventario.push(objeto.clonar());
     }
+
     /**
      * Suma puntos al jugador.
      * @param {number} cantidad
@@ -29,20 +39,25 @@ export class Jugador {
     sumarPuntos(cantidad) {
         this.puntos += cantidad;
     }
+
     /**
-     * Calcula el ataque total por armas del inventario.
+     * Calcula el ataque total: Base + Armas.
      * @returns {number}
      */
     obtenerAtaqueTotal() {
-        return this.inventario.reduce((acc, obj) => acc + (obj.tipo === 'arma' ? obj.bonus.ataque : 0), 0);
+        let bonoInventario = this.inventario.reduce((acc, obj) => acc + (obj.tipo === 'arma' ? obj.bonus.ataque : 0), 0);
+        return this.ataqueBase + bonoInventario;
     }
+
     /**
-     * Calcula la defensa total por armaduras del inventario.
+     * Calcula la defensa total: Base + Armaduras.
      * @returns {number}
      */
     obtenerDefensaTotal() {
-        return this.inventario.reduce((acc, obj) => acc + (obj.tipo === 'armadura' ? obj.bonus.defensa : 0), 0);
+        let bonoInventario = this.inventario.reduce((acc, obj) => acc + (obj.tipo === 'armadura' ? obj.bonus.defensa : 0), 0);
+        return this.defensaBase + bonoInventario;
     }
+
     /**
      * Calcula la vida total sumando efectos de consumibles.
      * @returns {number}
